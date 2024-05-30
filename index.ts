2f9efae72b9da338e498acb07aaf3e65ce6f11e0
gianluca cattin <gcattin97@gmail.com>
2024-05-30 00:12:25 -0500

    prntr go brrr

--- index.ts
+++ index.ts
@@ -1,70 +1,74 @@
 import { ENTRYPOINT_ADDRESS_V07, createSmartAccountClient, getRequiredPrefund, type UserOperation } from "permissionless"
 import { signerToSafeSmartAccount } from "permissionless/accounts"
 import {
-	createPimlicoBundlerClient,
-	createPimlicoPaymasterClient,
+  createPimlicoBundlerClient,
+  createPimlicoPaymasterClient,
 } from "permissionless/clients/pimlico"
-import { createPublicClient, http, parseEther, type Hex, erc20Abi, type Address, createWalletClient, stringToHex, encodeFunctionData } from "viem"
+import { createPublicClient, http, parseEther, getContract, type Hex, erc20Abi, type Address, createWalletClient, stringToHex, encodeFunctionData } from "viem"
 import { arbitrum } from "viem/chains"
 import { generatePrivateKey, privateKeyToAccount } from "viem/accounts"
 import { neethAbi } from "./neethAbi"
+import { factoryAbi } from "./factoryAbi"
 
 const NEETH_ADDRESS = "0x00000000000009B4AB3f1bC2b029bd7513Fbd8ED" as const;
+const FACTORY_ADDRESS = "0x53E60A0d769DD73CedF79041aE2cbB4f9af14a95" as const;
 const API_KEY = process.env.PIMLICO_API_KEY;
 
 let PRIVATE_KEY = process.env.PRIVATE_KEY as Hex;
 
 if (!PRIVATE_KEY) {
-    PRIVATE_KEY = generatePrivateKey();
+  PRIVATE_KEY = generatePrivateKey();
 }
 
 const pimlicoEndpoint = `https://api.pimlico.io/v2/arbitrum/rpc?apikey=${API_KEY}`;
 
 export const publicClient = createPublicClient({
-	transport: http("https://rpc.ankr.com/arbitrum/"),
-})
- 
-export const paymasterClient = createPimlicoPaymasterClient({
-	transport: http(`https://api.pimlico.io/v2/arbitrum/rpc?apikey=${API_KEY}`),
-	entryPoint: ENTRYPOINT_ADDRESS_V07,
-})
- 
-export const pimlicoBundlerClient = createPimlicoBundlerClient({
-	transport: http(`https://api.pimlico.io/v2/arbitrum/rpc?apikey=${API_KEY}`),
-	entryPoint: ENTRYPOINT_ADDRESS_V07,
+  transport: http("https://rpc.ankr.com/arbitrum/"),
 })
 
-const signer = privateKeyToAccount(PRIVATE_KEY); 
+export const paymasterClient = createPimlicoPaymasterClient({
+  transport: http(`https://api.pimlico.io/v2/arbitrum/rpc?apikey=${API_KEY}`),
+  entryPoint: ENTRYPOINT_ADDRESS_V07,
+})
+
+export const pimlicoBundlerClient = createPimlicoBundlerClient({
+  transport: http(`https://api.pimlico.io/v2/arbitrum/rpc?apikey=${API_KEY}`),
+  entryPoint: ENTRYPOINT_ADDRESS_V07,
+})
+
+const signer = privateKeyToAccount(PRIVATE_KEY);
 
 console.log('Signer:', signer.address)
 
 const walletClient = createWalletClient({
-    account: signer,
-    transport: http("https://rpc.ankr.com/arbitrum/"),
-    chain: arbitrum,
+  account: signer,
+  transport: http("https://rpc.ankr.com/arbitrum/"),
+  chain: arbitrum,
 })
 
+
+const factoryContract = getContract({
+  address: FACTORY_ADDRESS,
+  abi: factoryAbi,
+  client: {
+    public: publicClient,
+    wallet: walletClient,
+  }
+})
+
+
+
 const safeAccount = await signerToSafeSmartAccount(publicClient, {
-	entryPoint: ENTRYPOINT_ADDRESS_V07,
-	signer: signer,
-	saltNonce: 0n, // optional
-	safeVersion: "1.4.1",
-	// address: "0x...", // optional, only if you are using an already created account
+  entryPoint: ENTRYPOINT_ADDRESS_V07,
+  signer: signer,
+  saltNonce: 0n, // optional
+  safeVersion: "1.4.1",
+  // address:, // optional, only if you are using an already created account
 })
 
 console.log('Safe Account:', safeAccount.address)
 
-const depositTx = await walletClient.sendTransaction({
-    to: safeAccount.address,
-    value: parseEther("0.00001"), // amount to deposit
-    data: encodeFunctionData({
-        abi: neethAbi,
-        functionName: 'depositTo',
-        args: [safeAccount.address],
-    })
-})
 
-console.log('Deposit Tx:', depositTx)
 
 const smartAccountClient = createSmartAccountClient({
 	account: safeAccount,
@@ -85,7 +89,7 @@ const smartAccountClient = createSmartAccountClient({
 
             console.log('Required Prefund:', requiredPrefund)
 
-            // check neeth balance 
+            // check neeth balance
             const neethBalance = await publicClient.readContract({
                 address: NEETH_ADDRESS,
                 abi: erc20Abi,
@@ -101,10 +105,10 @@ const smartAccountClient = createSmartAccountClient({
 				})
 
                 console.log('Gas Estimates: (NEETH)', gasEstimates)
- 
+
 				return {
 					...gasEstimates,
-					paymaster: NEETH_ADDRESS, 
+					paymaster: NEETH_ADDRESS,
 				}
 			} else {
                 const gasEstimates = await pimlicoBundlerClient.estimateUserOperationGas({
@@ -122,10 +126,19 @@ const smartAccountClient = createSmartAccountClient({
 	},
 })
 
-const txHash = await smartAccountClient.sendTransaction({
-	to: "0xcaaa5473929bdd3321cf47cdc971bcbb91cf0313",
-	value: 0n,
-    data: stringToHex("NEETH is SAFE"),
-})
+// Encode the transaction data for creating the token
+// Change args as needed
+const deployTokenData = encodeFunctionData({
+  abi: factoryAbi,
+  functionName: "createToken",
+  args: ["Test Token", "TST", 100000000000000000000000n]
+});
 
-console.log('Tx Hash:', txHash)
+// Send the transaction through the smartAccountClient
+const txHash = await smartAccountClient.sendTransaction({
+  to: FACTORY_ADDRESS,
+  data: deployTokenData,
+  value: 0n,
+});
+
+console.log('Transaction Hash:', txHash)
